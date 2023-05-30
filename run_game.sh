a850fddf1946367a95292e9b6b3739f2ccbca0fd
Torsten Stremlau <torsten@stremlau.eu>
2023-05-30 23:30:52 +0200

    Add new level

--- run_game.sh
+++ run_game.sh
@@ -3,8 +3,8 @@
 # check if we run on wsl (Windows Subsystem for Linux)
 if [[ $(grep WSL /proc/version) ]]; then
     # we run on WSL
-    ./dosbox_launcher.sh run_game_windows.dosbox_config release "loadfix -150 game2 --german" "exit"
+    ./dosbox_launcher.sh run_game_windows.dosbox_config release "loadfix -100 game --german" "exit"
 else
     # we run on native Linux
-    ./dosbox_launcher.sh run_game.dosbox_config release "loadfix -150 game2 --level 15 4 --english" "exit"
+    ./dosbox_launcher.sh run_game.dosbox_config release "loadfix -100 game2 --level 17 4 --german" "exit"
 fi
