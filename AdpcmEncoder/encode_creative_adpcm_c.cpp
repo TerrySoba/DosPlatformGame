7bfac6622970e93f32d5a9dfb2ce1eb7dda670c8
Torsten Stremlau <torsten@stremlau.eu>
2023-08-25 22:36:45 +0200

    Optimize ADPCM encoder

--- AdpcmEncoder/encode_creative_adpcm_c.cpp
+++ AdpcmEncoder/encode_creative_adpcm_c.cpp
@@ -45,9 +45,6 @@ void storeFile(const std::string& filename, const std::vector<uint8_t>& data)
     fclose(fp);
 }
 
-#define clamp(value, smallest, largest) \
-    std::max((smallest), std::min((value), (largest)))
-
 
 /**
  * This class is a decoder for 4bit Creative ADPCM
@@ -72,13 +69,13 @@ public:
      */
     uint8_t decodeNibble(uint8_t nibble)
     {
-        int sign = (nibble & 8)?-1:1;               // Input is just 4 bits (a nibble), so the 4th bit is the sign bit
+        int sign = (nibble & 8) / 4 - 1;            // Input is just 4 bits (a nibble), so the 4th bit is the sign bit
         uint8_t data = nibble & 7;                  // The lower 3 bits are the sample data
         uint8_t delta = 
             (data * m_accumulator) +
             (m_accumulator / 2);                    // Scale sample data using accumulator value
         int result = m_previous + (sign * delta);   // Calculate the next value
-        m_previous = clamp(result, 0, 255);         // Limit value to 0..255
+        m_previous = std::clamp(result, 0, 255);    // Limit value to 0..255
 
         if ((data == 0) && (m_accumulator > 1))     // If input value is 0, and accumulator is
             m_accumulator /= 2;                     // larger than 1, then halve accumulator.
