c7d96e0f60753e56722eeb890ad6bfc62f43996c
Torsten Stremlau <torsten@stremlau.de>
2024-12-15 20:36:15 +0100

    Update build script for architecture-specific compressor and modify installation date

--- build_installer.sh
+++ build_installer.sh
@@ -5,4 +5,16 @@ DIR_NAME=installer
 
 rm $DIR_NAME/$ARCHIVE_NAME
 mkdir $DIR_NAME
-tools/compressor release/* $DIR_NAME/$ARCHIVE_NAME
+
+
+if [ "$(uname -m)" = "x86_64" ]
+then
+    COMPRESSOR_EXECUTABLE=tools/x86_64/compressor
+fi
+
+if [ "$(uname -m)" = "aarch64" ]
+then
+    COMPRESSOR_EXECUTABLE=tools/arm64/compressor
+fi
+
+$COMPRESSOR_EXECUTABLE release/* $DIR_NAME/$ARCHIVE_NAME
