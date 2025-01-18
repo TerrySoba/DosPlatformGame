e127cbc5effccc73226aed300343af8792d62c5c
Torsten Stremlau <torsten@stremlau.de>
2025-01-18 01:25:03 +0100

    Improve credits

---
+++ source/credits_loader.cpp
@@ -0,0 +1,60 @@
+#include "credits_loader.h"
+
+#include <stdio.h>
+#include <string.h>
+
+CreditsLoader::CreditsLoader(const char *filename)
+{
+    FILE *file = fopen(filename, "r");
+    if (!file)
+    {
+        throw Exception("Could not open file:", filename);
+    }
+
+    char line[256];
+    while (fgets(line, sizeof(line), file))
+    {
+        int len = strlen(line);
+        if (len > 0)
+        {
+            if (line[len - 1] == '\n')
+            {
+                line[len - 1] = 0;
+            }
+            CreditsEntry entry;
+            switch (line[0])
+            {
+            case '#':
+                entry.type = CREDITS_TYPE_ROLE;
+                entry.text = (line + 1);
+                break;
+            case '-':
+                entry.type = CREDITS_TYPE_SECTION_END;
+                entry.text = "";
+                break;
+            default:
+                entry.type = CREDITS_TYPE_PERSON;
+                entry.text = line;
+                break;
+            }
+
+            m_entries.push_back(entry);
+        }
+    }
+
+    fclose(file);
+}
+
+CreditsLoader::~CreditsLoader()
+{
+}
+
+CreditsEntry &CreditsLoader::getEntry(int index)
+{
+    return m_entries[index];
+}
+
+int CreditsLoader::size() const
+{
+    return m_entries.size();
+}
--- source/credits_loader.h
+++ source/credits_loader.h
@@ -6,9 +6,6 @@
 #include "exception.h"
 #include "tiny_string.h"
 
-#include <stdio.h>
-#include <string.h>
-
 enum CreditsType
 {
     CREDITS_TYPE_ROLE = 1,
@@ -26,67 +23,15 @@ struct CreditsEntry
 class CreditsLoader
 {
 public:
-    CreditsLoader(const char* filename)
-    {
-        FILE* file = fopen(filename, "r");
-        if (!file)
-        {
-            throw Exception("Could not open file:", filename);
-        }
+    CreditsLoader(const char* filename);
+    ~CreditsLoader();
 
-        char line[256];
-        while (fgets(line, sizeof(line), file))
-        {
-            int len = strlen(line);
-            if (len > 0)
-            {
-                if (line[len - 1] == '\n')
-                {
-                    line[len - 1] = 0;
-                }
-                CreditsEntry entry;
-                switch(line[0])
-                {
-                    case '#':
-                        entry.type = CREDITS_TYPE_ROLE;
-                        entry.text = (line + 1);
-                        break;
-                    case '-':
-                        entry.type = CREDITS_TYPE_SECTION_END;
-                        entry.text = "";
-                        break;
-                    default:
-                        entry.type = CREDITS_TYPE_PERSON;
-                        entry.text = line;
-                        break;
-                }
+    CreditsEntry& getEntry(int index);
 
-                m_entries.push_back(entry);
-            }
-        }
-        
-        fclose(file);
-    }
-
-    ~CreditsLoader()
-    {
-        
-    }
-
-    CreditsEntry& getEntry(int index)
-    {
-        return m_entries[index];
-    }
-
-    int size() const
-    {
-        return m_entries.size();
-    }
-
+    int size() const;
 
 private:
     tnd::vector<CreditsEntry> m_entries;
-
 };
 
 
