a37fe373aae48d80ce17a1c4e6528e42e7d15179
Torsten Stremlau <torsten.stremlau@gmail.com>
2025-01-20 16:04:26 +0100

    Update ubuntu_setup.sh to conditionally install packages based on architecture

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 apt update
-apt -y install git gcc g++ cmake nasm upx python3 zip p7zip-full mtools catch2
+
+REQUIRED_PACKAGES="git gcc g++ cmake nasm python3 zip p7zip-full mtools catch2"
+
+if [[ "$(uname -m)" = "x86_64" || "$(uname -m)" = "i686" ]]; then
+    REQUIRED_PACKAGES="$REQUIRED_PACKAGES upx"
+fi
+
+if [[ "$(uname -m)" = "aarch64" || "$(uname -m)" = "armv7l" ]]; then
+    REQUIRED_PACKAGES="$REQUIRED_PACKAGES docker.io dosbox"
+fi
+
+apt -y install $REQUIRED_PACKAGES
