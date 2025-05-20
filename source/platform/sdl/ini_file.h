5047b495873fa88d00d53a39dd41b1f9dc606c76
Torsten Stremlau <torsten@stremlau.de>
2025-05-20 21:02:47 +0200

    Add IniFile class for handling INI file operations and implement unit tests

---
+++ source/platform/sdl/ini_file.h
@@ -0,0 +1,25 @@
+#ifndef INI_FILE_H_INCLUDED
+#define INI_FILE_H_INCLUDED
+
+#include <string>
+#include <optional>
+#include <map>
+
+class IniFile
+{
+public:
+    IniFile(const std::string& fileName);
+    ~IniFile() = default;
+
+    std::optional<std::string> getString(const std::string& key);
+    void setString(const std::string& key, const std::string& value);
+
+    void save();
+
+private:
+    std::string m_fileName;
+    std::map<std::string, std::string> m_data;
+};
+
+
+#endif
---
+++ source/platform/sdl/ini_file_test.cpp
@@ -0,0 +1,31 @@
+#include "ini_file.h"
+
+#include "exception.h"
+#include "unit_test.h"
+
+
+TEST(IniFileTest_exception)
+{
+    ASSERT_THROW(
+        IniFile ini("unknown_file.ini"); ini.getString("name");,
+        Exception
+    );
+}
+
+TEST(IniFileTestUnix)
+{
+    IniFile ini(TEST_DATA_DIR "test_lf.ini");
+    ASSERT_TRUE(ini.getString("name") == "MyName");
+    ASSERT_TRUE(ini.getString("age") == "43");
+    ASSERT_TRUE(ini.getString("mail") == "Some@some.org");
+    ASSERT_TRUE(ini.getString("id") == "123456");
+}
+
+TEST(IniFileTestWindows)
+{
+    IniFile ini(TEST_DATA_DIR "test_crlf.ini");
+    ASSERT_TRUE(ini.getString("name") == "MyName");
+    ASSERT_TRUE(ini.getString("age") == "43");
+    ASSERT_TRUE(ini.getString("mail") == "Some@some.org");
+    ASSERT_TRUE(ini.getString("id") == "123456");
+}
