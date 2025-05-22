cb02f4920342259b51ed69c9e0ecde36b13d3344
Torsten Stremlau <torsten@stremlau.de>
2025-05-22 20:52:08 +0200

    Add build type specification and strip binaries in cross-build script

--- windows_build_docker/cross_build.sh
+++ windows_build_docker/cross_build.sh
@@ -4,9 +4,11 @@ set -e
 
 mkdir -p /build
 cd /build
-cmake -DCMAKE_TOOLCHAIN_FILE=/src/build/sdl/mingw-toolchain.cmake /src/build/sdl
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=/src/build/sdl/mingw-toolchain.cmake /src/build/sdl
 make -j4
 make install
+strip bin/*.dll
+strip bin/*.exe
 cd /
 zip -r game.zip /build/bin
 mkdir -p /src/result
