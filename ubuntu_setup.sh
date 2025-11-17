bb4c5f7af9f2cb3beb677eb19a58f3202674c349
Torsten Stremlau <torsten@stremlau.de>
2025-11-17 21:19:51 +0100

    Update ubuntu build script

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 apt update
 
+# Tools for building the dos variant
 REQUIRED_PACKAGES="git gcc g++ cmake nasm python3 zip p7zip-full mtools catch2"
+
+# Tools for building the SDL port
+REQUIRED_PACKAGES="$REQUIRED_PACKAGES ninja-build libsdl3-dev vorbis-tools"
 
 if [[ "$(uname -m)" = "x86_64" || "$(uname -m)" = "i686" ]]; then
     REQUIRED_PACKAGES="$REQUIRED_PACKAGES upx"
